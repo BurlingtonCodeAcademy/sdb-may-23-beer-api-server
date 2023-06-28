241a309ddfcc990d466da13e67b8c08a8c20465e
Paul Niemczyk <mrpndev@gmail.com>
2023-06-28 15:25:09 -0400

    Mongoose CRUD on auth + bcrypt

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,87 +1,61 @@
 const router = require("express").Router()
-// Import helper functions
-const { read, save } = require("../helper/rw")
-// Import path to json file
-const dbPath = "./db/users.json"
+const User = require("../models/User")
+const bcrypt = require("bcrypt")
+// value specifying how many times we run algorithm on the data to be scrambled
+const SALT = Number(process.env.SALT)
 
-router.post("/register", (req, res) => {
+router.post("/register", async (req, res) => {
     try {
-        // Destructure the body
         const { name, email, password } = req.body
-        // Grab current view of our json file
-        let userDB = read(dbPath)
-        // Check if user exists
-        let foundUser = userDB.filter(usr => usr.email === email)
-        // Throw error if user already exists
-        if (foundUser.length > 0) {
-            throw Error("Email already exists")
-        }
-        // Push data to read array if user exists
-        userDB.push({ name, email, password })
-        // Save the user to the json file
-        save(userDB, dbPath)
-        // Send a response
+
+        if (!name || !email || !password) throw Error("Incorrect schema values")
+
+        // Instantiate a new model using provided req.body object values
+        // Hash the password using .hashSync() with req.body.password and the SALT value
+        // Assign the password property to the value of .hashSync() return
+        const newUser = new User({ name, email, password: bcrypt.hashSync(password, SALT) })
+        // Save the model document into the collection
+        await newUser.save()
+
         res.status(201).json({
             message: `User created`,
-            email
-        })
-    } catch(err) {
-        res.status(500).json({
-            message: `${err}`,
-        })
-    }
-})
-
-
-// http://localhost:4000/auth/login
-router.post("/login", (req, res) => {
-    try {
-        // Get data from the request.body
-        const email = req.body.email
-        const password = req.body.password
-
-        // Read current data inside .json file
-        const db = read(dbPath)
-
-        // Check if the email from body matches email in .json file
-        const foundUser = db.filter(usr => usr.email === email)
-
-        // Error handling if no user is found
-        if (foundUser.length === 0) {
-            throw Error("No user found")
-            // ? OR response way in lieu of throwing exceptions
-            // res.status(403).json({
-            //     message: `User not found`
-            // })
-        }
-
-        // foundUser[0].password === password
-        //     ? res.status(200).json({
-        //         message: `User logged in`
-        //     })
-        //     : res.status(403).json({
-        //         message: `Incorrect password`
-        //     })
-
-        // Error handling if passwords do not match
-        if (foundUser[0].password !== password) {
-            throw Error("Incorrect credentials")
-        }
-
-        // Response if passwords do match
-        res.status(200).json({
-            message: `User logged in`
+            newUser
         })
 
     } catch(err) {
-        // All other error handling catchall
+        console.log(err)
         res.status(500).json({
             message: `${err}`
         })
     }
 })
 
+// http://localhost:4000/auth/login
+router.post("/login", async (req, res) => {
+    try {
+        const { email, password } = req.body
+        
+        let foundUser = await User.findOne({ email })
+        
+        if (!foundUser) throw Error("User not found")
 
+        // Async .compare() method which takes password from req.body
+        // Compares it against password from the user found in the db
+        const verifyPwd = await bcrypt.compare(password, foundUser.password)
 
+        if (!verifyPwd) throw Error("Incorrect password")
+
+        res.status(200).json({
+            message: `Logged in`,
+            foundUser
+        })
+
+    } catch(err) {
+        console.log(err)
+        res.status(500).json({
+            message: `${err}`
+        })
+    }
+})
 
 module.exports = router
--- controllers/routes.js
+++ controllers/routes.js
@@ -1,28 +1,25 @@
 const router = require("express").Router()
-const { read, save } = require("../helper/rw")
-const { v4: uuid_v4 } = require("uuid")
-const dbPath = "./db/beers.json"
+const Beer = require("../models/Beer")
 
-router.post("/create", (req, res) => {
+router.post("/create", async (req, res) => {
     try {
-        // Generate an ID
-        const id = uuid_v4()
-        // Get all items from the json file
-        const db = read(dbPath)
-        // Extrapolate the data from the request
-        // Check if the body has content
-        if (Object.keys(req.body).length < 6) {
-            throw Error("Please provide all content")
-        }
-        // Package id and the req data into a single object
-        let newEntry = { id, ...req.body }
-        // Push the new content into the db file
-        db.push(newEntry)
-        // Write new changes to the .json file
-        save(db, dbPath)
+
+        const newBeer = new Beer(req.body)
+
+        const saver = await newBeer.save()
+
+        res.status(201).json({
+            message: `Beer created`,
+            newBeer
+        })
 
     } catch(err) {
-        res.status(500).json({
+        console.log(err)
+        err.name === "ValidationError"
+        ? res.status(400).json({
+            message: `Provided data type invalid`
+        })
+        : res.status(500).json({
             message: `${err}`
         })
     }
@@ -30,8 +27,7 @@ router.post("/create", (req, res) => {
 
 router.get("/", (_, res) => {
     try {
-        const allBeers = read(dbPath)
-        res.status(200).json(allBeers)
+        
     } catch(err) {
         console.log(err)
         res.status(500).json({
@@ -42,17 +38,7 @@ router.get("/", (_, res) => {
 
 router.get("/:id", (req, res) => {
     try {
-        // Destructure the id value from the request
-        const { id } = req.params
-        // Get your json file contents
-        const db = read(dbPath)
-        // Find the matching id
-        const foundItem = db.find(beer => beer.id === id)
-    
-        // Error handle if no item has been found
-        if (!foundItem) throw Error("No item found")
-    
-        res.status(200).json(foundItem)
+        
     } catch (err) {
         console.log(err.message)
         res.status(500).json({
@@ -64,33 +50,7 @@ router.get("/:id", (req, res) => {
 
 router.put("/update/:id", (req, res) => {
     try {
-        // Destructure beer id from the param
-        const { id } = req.params
-        // Read the database
-        const db = read(dbPath)
-        // Find index at which the entry exists within the db
-        const found = db.findIndex(beer => beer.id === id)
-        
-        // Error handling if findIndex doesn't find anything (returns -1)
-        if (found === -1) throw Error(`${id} not found`)
-        
-        // Reassign the values of properties inside db at found index
-        // ! IF the replacement values exist in the body
-        // If their value is falsey, keep existind db values
-        db[found].brand = req.body.brand ?? db[found].brand
-        db[found].brewery = req.body.brewery ?? db[found].brewery
-        db[found].abv = req.body.abv ?? db[found].abv
-        db[found].country = req.body.country ?? db[found].country
-        db[found].style = req.body.style ?? db[found].style
-        db[found].size = req.body.size ?? db[found].size
 
-        // Save to db
-        save(db, dbPath)
-        
-        res.status(200).json({
-            message: `Updated data at index of ${found}`,
-            data: db[found]
-        })
     } catch(err) {
         console.log(err)
         res.status(500).json({
@@ -102,18 +62,6 @@ router.put("/update/:id", (req, res) => {
 
 router.delete("/delete/:id", (req, res) => {
     try {
-        console.log("route hit")
-        const { id } = req.params
-        const db = read(dbPath)
-        const updatedDb = db.filter(beer => beer.id !== id)
-
-        if (db.length === updatedDb.length) throw Error(`${id} not found`)
-
-        save(updatedDb, dbPath)
-
-        res.status(200).json({
-            message: `Database updated`
-        })
 
     } catch(err) {
         console.log(err)
